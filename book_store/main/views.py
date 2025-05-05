2217cafa4bdd7874d5b73ce14d463c982f5e8d6a
wzy <23307130392@m.fudan.edu.cn>
2025-05-05 13:37:13 +0800

    加入urls.py

--- book_store/main/views.py
+++ book_store/main/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': '用户名或密码错误'})
+    return render(request, 'login.html')
